d6fb44945398e0dea27cbda1d3954aa207bd0b8b
Leaves_awa <bzbz233nb@163.com>
2026-05-30 13:46:41 +0800

    update？！真的假的

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,7 @@
 // 设置默认主题为 dark
-(function(){
-    var theme = localStorage.getItem("theme");
-    if(!theme || theme === "light"){
-        localStorage.setItem("theme", "dark");
-    }
-})();
+// (function(){
+//     var theme = localStorage.getItem("theme");
+//     if(!theme || theme === "light"){
+//         localStorage.setItem("theme", "dark");
+//     }
+// })();
